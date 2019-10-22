82ab138138129905550677bfdb59083e0ee7d83e
Brian Thorne <brian@thorne.link>
2019-10-22 15:23:51 +1100

    Refactor get_config into own function and add tests

--- blocklib/pprlpsig.py
+++ blocklib/pprlpsig.py
@@ -1,5 +1,7 @@
 import hashlib
-from .pprlindex import PPRLIndex
+
+from blocklib.configuration import get_config
+from blocklib.pprlindex import PPRLIndex
 
 
 class PPRLIndexPSignature(PPRLIndex):
@@ -19,25 +21,12 @@ class PPRLIndexPSignature(PPRLIndex):
             Configuration for P-Sig reverted index.
 
         """
-        self.num_hash_funct = self.get_config(config,'num_hash_funct')
-        self.attr_select_list = self.get_config(config,'attr_select_list')
-        self.bf_len = self.get_config(config,'bf_len')
-        self.min_occur_ratio = self.get_config(config,'min_occur_ratio')
-        self.max_occur_ratio = self.get_config(config,'max_occur_ratio')
-
-    def get_config(self, config, arg_name):
-        """Get arg value if arg_name exists in the config.
-
-        Arguments
-        ---------
-        config: dict
-        arg_name: str
-
-        """
-        value = config.get(arg_name, 'not specified')
-        if value == 'not specified':
-            raise ValueError('P-Sig: Argument "{}" is not specified'.format(arg_name))
-        return value
+        super().__init__()
+        self.num_hash_funct = get_config(config, 'num_hash_funct')
+        self.attr_select_list = get_config(config, 'attr_select_list')
+        self.bf_len = get_config(config, 'bf_len')
+        self.min_occur_ratio = get_config(config, 'min_occur_ratio')
+        self.max_occur_ratio = get_config(config, 'max_occur_ratio')
 
     def build_inverted_index(self, data, rec_id_col=None):
         """Build inverted index given P-Sig method."""
@@ -60,8 +49,7 @@ class PPRLIndexPSignature(PPRLIndex):
         # Filter revert index based on ratio
         n = len(data)
         invert_index = {k: v for k, v in invert_index.items()
-                        if len(v) < n * self.max_occur_ratio and
-                           len(v) > n * self.min_occur_ratio}
+                        if n * self.max_occur_ratio > len(v) > n * self.min_occur_ratio}
         if len(invert_index) == 0:
             raise ValueError('P-Sig: All records are filtered out!')
 
