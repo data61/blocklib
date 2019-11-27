0c2e044f3eb6b7d8f64dccdf572defdfd2d91d79
Guillaume Smith <gwil1248@fastmail.net>
2019-11-27 15:27:08 +1100

    Remove the PPRLIndex attribute `revert_index` which was unused.

--- tests/test_pprlindex.py
+++ tests/test_pprlindex.py
@@ -8,7 +8,6 @@ def test_init():
     assert pprl.rec_dict is None
     assert pprl.ent_id_col is None
     assert pprl.rec_id_col is None
-    assert pprl.revert_index == {}
     assert pprl.stats == {}
 
 
