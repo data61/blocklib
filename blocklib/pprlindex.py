012ed0785ca0eb4623c4820af435fd0deeaa16e8
Wilko Henecka <wilko.henecka@data61.csiro.au>
2021-03-10 21:27:51 +1100

    build_reversed_index returns a ReversedIndexResult now, removed summarize_reversed_index function

--- blocklib/pprlindex.py
+++ blocklib/pprlindex.py
@@ -1,7 +1,6 @@
 import random
-from typing import Any, Dict, List, Sequence, Optional
+from typing import Dict, List, Sequence, Optional
 from blocklib.configuration import get_config
-from blocklib.stats import reversed_index_stats
 from blocklib.utils import check_header
 
 
@@ -13,12 +12,13 @@ class PPRLIndex:
         self.rec_dict = None
         self.ent_id_col = None
         self.rec_id_col = None
-        self.stats = {}  # type: Dict[str, Any]
 
     def get_feature_to_index_map(self, data: Sequence[Sequence], header: Optional[List[str]] = None):
         """Return feature name to feature index mapping if there is a header and feature is of type string."""
         feature_type = type(self.blocking_features[0])  # type: ignore
         feature_to_index = None
+        if len(data) == 0:
+            return feature_to_index
         tuple_type = type(data[0])  # if data is CLKs, then tuple_type will be str, otherwise a tuple
 
         if header and feature_type == str and tuple_type != str:
@@ -39,42 +39,15 @@ class PPRLIndex:
         else:
             self.blocking_features_index = blocking_features
 
-    def build_reversed_index(self, data: Sequence[Sequence], verbose: bool, header: Optional[List[str]]  = None):
+    def build_reversed_index(self, data: Sequence[Sequence],  header: Optional[List[str]] = None):
         """Method which builds the index for all database.
 
            :param data: list of tuples, PII dataset
-           :param verbose: print additional information to std out.
            :param header: file header, optional
-
+           :rtype: ReversedIndexResult
            See derived classes for actual implementations.
         """
         raise NotImplementedError("Derived class needs to implement")
-
-    def summarize_reversed_index(self, reversed_index: Dict):
-        """Summarize statistics of reverted index / blocks."""
-        assert len(reversed_index) > 0
-        # statistics of block
-        self.stats = reversed_index_stats(reversed_index)
-        # find how many blocks each entity / record is a member of
-        rec_to_block = {}  # type: Dict[Any, List[Any]]
-        for block_id, block in reversed_index.items():
-            for rec in block:
-                if rec in rec_to_block:
-                    rec_to_block[rec].append(block_id)
-                else:
-                    rec_to_block[rec] = [block_id]
-        num_of_blocks_per_rec = [len(x) for x in rec_to_block.values()]
-        self.stats['num_of_blocks_per_rec'] = num_of_blocks_per_rec
-
-        print('Statistics for the generated blocks:')
-        print('\tNumber of Blocks:   {}'.format(self.stats['num_of_blocks']))
-        print('\tMinimum Block Size: {}'.format(self.stats['min_size']))
-        print('\tMaximum Block Size: {}'.format(self.stats['max_size']))
-        print('\tAverage Block Size: {}'.format(self.stats['avg_size']))
-        print('\tMedian Block Size:  {}'.format(self.stats['med_size']))
-        print('\tStandard Deviation of Block Size:  {}'.format(self.stats['std_size']))
-
-        return self.stats
 
     def select_reference_value(self, reference_data: Sequence[Sequence], ref_data_config: Dict):
         """Load reference data for methods need reference."""
@@ -92,3 +65,13 @@ class PPRLIndex:
 
         print('  Selected %d random reference values' % (len(ref_val_list)))
         return ref_val_list
+
+
+class ReversedIndexResult(object):
+
+    def __init__(self, reversed_index: Dict, stats: Dict):
+        self.reversed_index = reversed_index
+        self.stats = stats
+
+    def __eq__(self, other):
+        return self.reversed_index == other.reversed_index and self.stats == other.stats
